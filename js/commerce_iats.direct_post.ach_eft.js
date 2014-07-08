81131ac331373c527fdcfcafc4fe68410846f9d6
Dan Ruscoe <danruscoe@gmail.com>
2014-07-08 13:58:06 -0700

    Direct Post ACH/EFT form UI improvements.

---
+++ js/commerce_iats.direct_post.ach_eft.js
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * Commerce iATS Direct Post ACH/EFT JavaScript functionality.
+ */
+
+(function ($) {
+
+  Drupal.behaviors.commerce_iats_direct_post_ach_eft = {
+    attach: function(context, settings) {
+      $('input#edit-continue', context).unbind('click').bind('click', function() {
+
+        // General fields.
+
+        var account_number = $("input[name=internal_iats_dpm_number]").val();
+        var type = $("select[name=internal_iats_dpm_type]").val();
+
+        // USD currency fields.
+
+        var routing_number = $("input[name=internal_iats_dpm_routing_number]").val();
+
+        $("input[name=IATS_DPM_AccountNumber]").val(String(routing_number) + String(account_number));
+
+        // CAD currency fields.
+
+        var bank_number = $("input[name=internal_iats_dpm_bank_number]").val();
+        var transit_number = $("input[name=internal_iats_dpm_transit_number]").val();
+
+        $("input[name=IATS_DPM_AccountNumber]").val(String(bank_number) + String(transit_number) + String(account_number));
+
+        // GBP currency fields.
+
+        var sort_code = $("input[name=internal_iats_dpm_sort_code]").val();
+
+        $("input[name=IATS_DPM_AccountNumber]").val(String(sort_code) + String(account_number));
+
+        if (type) {
+          $("input[name=IATS_DPM_AccountType]").val(type);
+        }
+
+        return true;
+      });
+    }
+  }
+
+})(jQuery);
