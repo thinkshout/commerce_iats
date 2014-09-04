529e64330f573152b8e5de16a9a3b89d2f124b8e
Dan Ruscoe <danruscoe@gmail.com>
2014-09-04 10:55:12 -0700

    Better handling of card data form field focus.

--- modules/commerce_iats_usb_reader/js/commerce_iats_usb_reader.credit_card.js
+++ modules/commerce_iats_usb_reader/js/commerce_iats_usb_reader.credit_card.js
@@ -6,11 +6,31 @@
 (function ($) {
 
   Drupal.behaviors.commerce_iats_usb_reader_credit_card = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       var encrypted_element = $("textarea#edit-payment-details-encrypted-credit-card-encrypted-number");
 
+      // When the card data field has focus, instuct the user to swipe the card.
+      encrypted_element.focusin(function () {
+        $("#edit-payment-details-encrypted-credit-card-status").html('Swipe credit card now.');
+      });
+
+      // When the card data field loses focus, instruct the user to regain focus.
+      encrypted_element.focusout(function (e) {
+        $("#edit-payment-details-encrypted-credit-card-status").html('<a href="#" onclick="Drupal.behaviors.commerce_iats_usb_reader_credit_card.card_focus(); return false">Click to swipe credit card</a>');
+      });
+
       encrypted_element.focus();
+    },
+
+    card_focus: function () {
+      var encrypted_element = $("textarea#edit-payment-details-encrypted-credit-card-encrypted-number");
+      encrypted_element.focus();
+
+      // The focus event set in attach() is not caught when setting focus in JS.
+      // Have to set status text manually here.
+      $("#edit-payment-details-encrypted-credit-card-status").html('Swipe credit card now.');
     }
+
   }
 
 })(jQuery);
