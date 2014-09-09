9423ee5a8e909d21d5cf19d385d201840b4227a6
Lev Tsypin <lev@thinkshout.com>
2014-09-09 13:01:40 -0700

    Refine javascript toggles for card reader form elements.

--- modules/commerce_iats_usb_reader/js/commerce_iats_usb_reader.credit_card.js
+++ modules/commerce_iats_usb_reader/js/commerce_iats_usb_reader.credit_card.js
@@ -8,29 +8,29 @@
   Drupal.behaviors.commerce_iats_usb_reader_credit_card = {
     attach: function (context, settings) {
       var encrypted_element = $("textarea#edit-payment-details-encrypted-credit-card-encrypted-number", context);
+      var $status_element = $("#edit-payment-details-encrypted-credit-card-status", context);
 
       // When the card data field has focus, instuct the user to swipe the card.
       encrypted_element.focusin(function () {
-        $("#edit-payment-details-encrypted-credit-card-status", context).html(Drupal.t('Swipe credit card now.'));
+        $status_element.html(Drupal.t('Swipe credit card now.'));
       });
 
       // When the card data field loses focus, instruct the user to regain focus.
       encrypted_element.focusout(function (e) {
-        $("#edit-payment-details-encrypted-credit-card-status", context).html('<a href="#" onclick="Drupal.behaviors.commerce_iats_usb_reader_credit_card.card_focus(context); return false">Drupal.t(Click to swipe credit card)</a>');
+        // Create a swipe link with a defined click event.
+        var swipe_link = $('<a></a>').attr('href', '#').html(Drupal.t('Click to swipe credit card')).click(function(event) {
+          event.preventDefault();
+          encrypted_element.focus();
+
+          // The focus event set in attach() is not caught when setting focus in JS. Have to set status text manually here.
+          $status_element.html(Drupal.t('Swipe credit card now.'));
+        });
+
+        $status_element.html(swipe_link);
       });
 
       encrypted_element.focus();
-    },
-
-    card_focus: function (context) {
-      var encrypted_element = $("textarea#edit-payment-details-encrypted-credit-card-encrypted-number", context);
-      encrypted_element.focus();
-
-      // The focus event set in attach() is not caught when setting focus in JS.
-      // Have to set status text manually here.
-      $("#edit-payment-details-encrypted-credit-card-status", context).html(Drupal.t('Swipe credit card now.'));
     }
-
   }
 
 })(jQuery);
